7459c3a95c17791188c5c4e24714ebcbb7fcb952
jziz <zizzo.jacopo@gmail.com>
2022-05-01 16:27:46 +0200

    /

--- isolrun.m
+++ isolrun.m
@@ -1,13 +1,13 @@
 clear all
 close all
 %rng(69,'twister')
-Y=readmatrix('DG2-Martinengo-Maggio-1m-tagliato.csv');
-X=readmatrix('Potenza_DG2_Martinengo_1Maggio.csv');
+Y=readmatrix('"ydata".csv');
+X=readmatrix('"xdata".csv');
 Y_cusc_x=Y(:,1);
 
 Z=[X Y_cusc_x];
 %Z=readmatrix('breastw_683.csv');
-forest = iForest(Z, 100,  256, 8, 1); %0 non ext, 1 ext ("funziona" solo extended per ora)
+forest = iForest(Z, 100,  256, 8, 1); %0 non ext, 1 ext (only the "extended" version is working)
 Scores = anomaly_scores(Z,100,forest,256);
 
 Scores=rescale(Scores); %rescale between 0 and 1
